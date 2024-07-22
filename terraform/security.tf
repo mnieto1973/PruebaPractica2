95b388132b0465404232ff12ea831d4b58197b4f
Mariano Nieto <mnieto73@hotmail.com>
2024-07-22 17:19:38 +0200

    Comentarios

--- terraform/security.tf
+++ terraform/security.tf
@@ -3,7 +3,7 @@ resource "azurerm_network_security_group" "securityGroup" {
   name                = "securityGroupPruebaPractica2"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-
+# Puerto 22 SSH
   security_rule {
     name                       = "SSH"
     priority                   = 1001
@@ -15,7 +15,7 @@ resource "azurerm_network_security_group" "securityGroup" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
+# Puerto 80
   security_rule {
     name                       = "HTTP"
     priority                   = 1002
@@ -27,7 +27,7 @@ resource "azurerm_network_security_group" "securityGroup" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
+# Puerto 443
   security_rule {
     name                       = "HTTPS"
     priority                   = 1003
@@ -44,6 +44,7 @@ resource "azurerm_network_security_group" "securityGroup" {
     source = "PR2"
   }
 }
+# Se vincula el rol para hacer recuperar las imagenes del repositorio de ACR al AKS creado
 resource "azurerm_role_assignment" "aks_acr_pull" {
   principal_id   = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name = "AcrPull"
